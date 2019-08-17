994e34417e212ceb72eb73d13a8ac862ca8abf4d
anil235476 <aks.dce.007@gmail.com>
2019-08-17 10:14:30 +0530

    intial commit

---
+++ video_receiver_helper.cpp
@@ -0,0 +1,19 @@
+#include "video_receiver_helper.h"
+#include "video_receiver/video_track_receiver_impl.h"
+#include "media_render_util/video_render_util.h"
+
+namespace grt {
+
+	std::unique_ptr< video_track_receiver>
+		set_video_renderer(webrtc::MediaStreamTrackInterface* stream, grt::sender* sender, std::string const& id) {
+		auto track_receiver = get_receiver(stream);
+		util::async_set_video_renderer(track_receiver.get(), sender, id);
+		return track_receiver;
+	}
+
+	void 
+		async_reset_video_renderer(grt::sender* sender, std::string const& id) {
+		util::async_reset_video_renderer(sender, id);
+	}
+
+}//namespace grt
---
+++ video_receiver_helper.h
@@ -0,0 +1,21 @@
+#ifndef _VIDEO_RECEIVER_HELPER_H___
+#define _VIDEO_RECEIVER_HELPER_H___
+#include <memory>
+#include <string>
+
+namespace webrtc {
+	class MediaStreamTrackInterface;
+}
+
+namespace grt {
+	class video_track_receiver;
+	class sender;
+
+	std::unique_ptr< video_track_receiver> 
+		set_video_renderer(webrtc::MediaStreamTrackInterface*, grt::sender* sender, std::string const& id);
+
+	void async_reset_video_renderer(grt::sender* sender, std::string const& id);
+
+}//namespace grt
+
+#endif//_VIDEO_RECEIVER_HELPER_H___
