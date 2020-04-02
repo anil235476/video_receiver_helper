a5292a563007703b96b46f33de93664cb59b5395
anil235476 <aks.dce.007@gmail.com>
2020-04-02 15:27:07 +0530

    adding share toggle support

--- media_receiver.cpp
+++ media_receiver.cpp
@@ -48,6 +48,7 @@ namespace grt {
 		sender_->register_for_message(mic_toggle_id, callback);
 		sender_->register_for_message(cam_toggle_id, callback);
 		sender_->register_for_message(error_id, callback);
+		sender_->register_for_message(share_toggle_id, callback);
 		return sender_;
 	}
 
