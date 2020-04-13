876508011f30a5ceb754f4da639baf0213f18e11
anil235476 <aks.dce.007@gmail.com>
2020-04-13 20:39:17 +0530

    support of scalling and yuv to rgb in single api

--- video_receiver/video_track_receiver_impl.cpp
+++ video_receiver/video_track_receiver_impl.cpp
@@ -16,19 +16,74 @@ namespace util {
 		return grt::yuv_frame{ y, u, v, stridey, strideu, stridev, width, height };
 	}
 
+	std::pair<int,int> get_16_multiple(int const width, int const height) {
+		int const width_dif = width % 16;
+		int const height_diff = height % 16;
+		return std::make_pair(width - width_dif, height - height_diff);
+	}
+
 	grt::rgb_frame convert_to_rgb_frame(const webrtc::VideoFrame& frame) {
-		const auto width = frame.width();
-		const auto height = frame.height();
-		int stride = width;//todo: we need to work on this. for some resolution it does not work
-		/*if (width % 2) {
-			stride++;
-		}*/
+		const auto w_h = get_16_multiple(frame.width(), frame.height());
+		const auto width = w_h.first;
+		const auto height = w_h.second;
+		
 		rtc::scoped_refptr<webrtc::I420BufferInterface> yuv = frame.video_frame_buffer()->GetI420();
 		std::vector<uint8_t> data(width*height * 4, 0);
 		assert(data.size() == width * height * 4);
-		const auto r = libyuv::I420ToARGB(yuv->DataY(), yuv->StrideY(), yuv->DataU(), yuv->StrideU(),
-			yuv->DataV(), yuv->StrideV(), &data[0], stride *4, width, height);
-		assert(r == 0);
+		const auto r3= libyuv::YUVToARGBScaleClip(yuv->DataY(),
+								yuv->StrideY(),
+								yuv->DataU(),
+								yuv->StrideU(),
+								yuv->DataV(),
+								yuv->StrideV(),
+								libyuv::FourCC::FOURCC_I420,
+								frame.width(),
+								frame.height(),
+								&data[0],
+								width*4,
+								libyuv::FourCC::FOURCC_ARGB,
+								width,
+								height,
+								0,
+								0,
+								width,
+								height,
+								libyuv::FilterModeEnum::kFilterNone);
+		assert(r3 == 0);
+		/*
+		I am keeping below code for reference.
+		as still there is bug for resolution,
+		rendering is not right for
+		original resolution 1360, 768
+
+
+		destination resolution 1366, 768
+
+		*/
+		//const auto r = libyuv::I420ToARGB(yuv->DataY(), yuv->StrideY(), yuv->DataU(), yuv->StrideU(),
+		//	yuv->DataV(), yuv->StrideV(), &data[0], width *4, width, height);
+		//assert(r == 0);
+		//const auto new_dim = get_16_multiple(width, height);
+		//if (new_dim.first != width || new_dim.second != height) {
+
+		//	const auto new_width = new_dim.first;
+		//	const auto new_height = new_dim.second;
+
+		//	std::vector<uint8_t> dest_(new_width * new_height * 4, 0); 
+		//	//todo: it can be optimized also, new buffer requirement should not be there..
+		//	const auto r2 = libyuv::ARGBScale(&data[0],
+		//		width * 4,
+		//		width,
+		//		height,
+		//		&dest_[0],
+		//		new_width * 4,
+		//		new_width,
+		//		new_height,
+		//		libyuv::FilterModeEnum::kFilterNone);
+		//	assert(r2 == 0);
+		//	return grt::rgb_frame{ dest_ , new_width, new_height };
+		//}
+		//
 		return grt::rgb_frame{ data , width, height };
 		
 	}
